93be336d287a01c7c0310a35e09bfefa99132d3e
rijo01 <Rickard.ih.johansson@gmail.com>
2026-03-20 14:31:43 +0100

    fix: CSP headers för Supabase

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://*.supabase.co; img-src 'self' data:;"
+          }
+        ]
+      }
+    ]
+  }
+}
 module.exports = nextConfig
